5e4381ef9c345bf918afe1289d140ac73b193259
Hyunje Jun <me@noraesae.net>
2017-02-12 22:12:24 +0900

    Implement Stream instance for list

--- lib/Common/Stream.hs
+++ lib/Common/Stream.hs
@@ -1,3 +1,4 @@
+{-# LANGUAGE FlexibleInstances #-}
 {-# LANGUAGE FunctionalDependencies #-}
 {-# LANGUAGE MultiParamTypeClasses #-}
 module Common.Stream where
@@ -6,3 +7,7 @@ import Protolude
 
 class Stream s a | s -> a, a -> s where
   read :: s -> Maybe (a, s)
+
+instance Stream [a] a where
+  read (x:xs) = Just (x, xs)
+  read [] = Nothing
