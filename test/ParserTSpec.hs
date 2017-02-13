5f60e0538ecfb995dec7d6a90b72499c12a5623e
Hyunje Jun <me@noraesae.net>
2017-02-13 18:55:46 +0900

    Add an empty test for try of ParserT

--- test/ParserTSpec.hs
+++ test/ParserTSpec.hs
@@ -9,12 +9,21 @@ import Test.Hspec
 newtype E = E Text deriving (Show, Typeable)
 instance Exception E
 
-type Parser = ParserT [Integer] E Identity
+type Parser m = ParserT [Integer] E m
 
 spec :: Spec
 spec = describe "ParserT" $ do
-  it "execParserT" $ do
-    execParserT parser [1, 2, 3] `shouldBe` Identity (1, 2, 3)
-    where
-    parser :: Parser (Integer, Integer, Integer)
-    parser = (,,) <$> next <*> next <*> next
+  it "execParserT" $
+    let
+      parser :: Parser Identity (Integer, Integer, Integer)
+      parser = (,,) <$> next <*> next <*> next
+    in
+      execParserT parser [1, 2, 3] `shouldBe` Identity (1, 2, 3)
+
+  it "try" $
+    let
+      tryTest :: Monad m => Parser m ()
+      tryTest = do
+        return ()
+    in
+      execParserT tryTest [1..] :: Expectation
