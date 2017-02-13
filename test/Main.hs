bf4d7ffa12816ec00aac594174ba51f2fa5edb22
Hyunje Jun <me@noraesae.net>
2017-02-13 13:14:00 +0900

    Add test for ParserT

--- test/Main.hs
+++ test/Main.hs
@@ -4,9 +4,11 @@ import Protolude
 
 import Test.Hspec
 
+import qualified ParserTSpec as ParserT
 import qualified LexerSpec as Lexer
 import qualified ParserSpec as Parser
 
 main :: IO ()
-main = hspec $ Lexer.spec
+main = hspec $ ParserT.spec
+            >> Lexer.spec
             >> Parser.spec
---
+++ test/ParserTSpec.hs
@@ -0,0 +1,12 @@
+module ParserTSpec where
+
+import Protolude
+
+import Common.ParserT
+
+import Test.Hspec
+
+spec :: Spec
+spec = describe "ParserT" $ do
+  it ".." $ do
+    1 `shouldBe` 1
