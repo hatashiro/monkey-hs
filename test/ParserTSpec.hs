7dc1e90210b0a0cf1d8533bc0fc90831a76e5542
Hyunje Jun <me@noraesae.net>
2017-02-13 13:22:33 +0900

    execParserT test

--- test/ParserTSpec.hs
+++ test/ParserTSpec.hs
@@ -6,7 +6,15 @@ import Common.ParserT
 
 import Test.Hspec
 
+newtype E = E Text deriving (Show, Typeable)
+instance Exception E
+
+type Parser = ParserT [Integer] E Identity
+
 spec :: Spec
 spec = describe "ParserT" $ do
-  it ".." $ do
-    1 `shouldBe` 1
+  it "execParserT" $ do
+    execParserT parser [1, 2, 3] `shouldBe` Identity (1, 2, 3)
+    where
+    parser :: Parser (Integer, Integer, Integer)
+    parser = (,,) <$> next <*> next <*> next
