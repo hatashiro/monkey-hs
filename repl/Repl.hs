57383467c841b776cd3ddb73eb8c7282e0035f61
Hyunje Jun <me@noraesae.net>
2017-03-02 02:48:31 +0900

    Prevent REPL from crashing for invalid input

--- repl/Repl.hs
+++ repl/Repl.hs
@@ -1,35 +1,38 @@
 {-# LANGUAGE ScopedTypeVariables #-}
 module Repl where
 
-import Protolude hiding (evaluate)
+import Protolude hiding (evaluate, catch)
 
+import Control.Exception (catch)
 import Data.Text (pack)
 import Lexer (lex)
 import Parser (parse)
 import Common.ParserT (ParserError)
 import System.Console.Haskeline hiding (catch)
 
-loop :: MonadIO m => m a -> m a
+loop :: IO a -> IO a
 loop io = io >> loop io
 
-read :: (MonadException m, MonadIO m) => m (Maybe Text)
+read :: IO (Maybe Text)
 read = runInputT defaultSettings $ (fmap . fmap) pack $ getInputLine "> "
 
-evaluate :: MonadIO m => Text -> m Text
+evaluate :: Text -> Either ParserError Text
 evaluate input = do
-  let tokens = lex input
-  let ast = parse tokens
+  tokens <- lex input
+  ast <- parse tokens
   return $ show ast
 
-rep :: (MonadException m, MonadIO m) => m ()
+rep :: IO ()
 rep = do
   maybeText <- read
   case maybeText of
     Just "" -> return ()
     Nothing -> return ()
     Just text -> do
-      result <- evaluate text
-      putStrLn result
+      let result = evaluate text
+      putStrLn $ case result of
+        Left err -> show err
+        Right result -> result
 
 repl :: IO ()
 repl = loop rep
