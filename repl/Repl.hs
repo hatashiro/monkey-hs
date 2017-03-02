f642b58273eca873998d0e1a02b352438f49cb89
Hyunje Jun <me@noraesae.net>
2017-03-03 00:31:54 +0900

    Update REPL to actually evaluate the result

--- repl/Repl.hs
+++ repl/Repl.hs
@@ -1,14 +1,17 @@
 {-# LANGUAGE ScopedTypeVariables #-}
 module Repl where
 
-import Protolude hiding (evaluate, catch)
+import Protolude hiding (evaluate)
 
-import Control.Exception (catch)
-import Data.Text (pack)
-import Lexer (lex)
-import Parser (parse)
-import Common.ParserT (ParserError)
-import System.Console.Haskeline hiding (catch)
+import           Data.Text (pack)
+import           Lexer (lex)
+import           Parser (parse)
+import           Evaluator (eval)
+import           Evaluator.Types (EvalError)
+import           Evaluator.Object (Object)
+import           Common.ParserT (ParserError)
+import           System.Console.Haskeline
+import qualified GHC.Show as G
 
 loop :: IO a -> IO a
 loop io = io >> loop io
@@ -16,11 +19,16 @@ loop io = io >> loop io
 read :: IO (Maybe Text)
 read = runInputT defaultSettings $ (fmap . fmap) pack $ getInputLine "> "
 
-evaluate :: Text -> Either ParserError Text
+data InterpretError = P ParserError | E EvalError
+
+instance G.Show InterpretError where
+  show (P p) = show p
+  show (E p) = show p
+
+evaluate :: Text -> Either InterpretError Object
 evaluate input = do
-  tokens <- lex input
-  ast <- parse tokens
-  return $ show ast
+  ast <- first P $ lex input >>= parse
+  first E $ eval ast
 
 rep :: IO ()
 rep = do
@@ -31,8 +39,8 @@ rep = do
     Just text -> do
       let result = evaluate text
       putStrLn $ case result of
-        Left err -> show err
-        Right result -> result
+        Left err -> (show err :: Text)
+        Right object -> show object
 
 repl :: IO ()
 repl = loop rep
