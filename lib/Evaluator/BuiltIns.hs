0d65c6cb11499e59097f26742191b63dfa0a1c75
Hyunje Jun <me@noraesae.net>
2017-03-05 14:48:22 +0900

    Implement built-in print function

--- lib/Evaluator/BuiltIns.hs
+++ lib/Evaluator/BuiltIns.hs
@@ -17,6 +17,15 @@ len = OBuiltInFn "len" 1 go
   go [OString t] = return . OInt . fromIntegral $ T.length t
   go args = invalid len args
 
+bprint :: Object
+bprint = OBuiltInFn "print" 1 go
+  where
+  go :: [Object] -> IO Object
+  go [OString t] = putStrLn t $> nil
+  go [o] = putStrLn (show o :: Text) $> nil
+  go args = invalid bprint args
+
 builtIns :: [(Ident, Object)]
 builtIns = [ (Ident "len", len)
+           , (Ident "print", bprint)
            ]
