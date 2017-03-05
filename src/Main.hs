99b4db4c29795e6bb33373e659fad9c56ffd903a
Hyunje Jun <me@noraesae.net>
2017-03-05 18:06:29 +0900

    Implement mki, the main interpreter

--- src/Main.hs
+++ src/Main.hs
@@ -1,6 +1,29 @@
 module Main where
 
-import Protolude
+import Protolude hiding (evaluate)
+
+import           Common.ParserT (ParserError)
+import           Evaluator (eval)
+import           Lexer (lex)
+import           Parser (parse)
+import           System.Environment (getArgs)
 
 main :: IO ()
-main = putStrLn ("Hello, Haskell!" :: Text)
+main = getArgs >>= traverse_ evalFile
+
+evalFile :: FilePath -> IO ()
+evalFile file = readFile file >>= evaluate
+
+printError :: Exception e => e -> IO ()
+printError e = putStrLn (show e :: Text)
+
+evaluate :: Text -> IO ()
+evaluate input = do
+  let parsed = lex input >>= parse
+  case parsed of
+    Left err -> printError err
+    Right ast -> do
+      evaluated <- eval ast
+      case evaluated of
+        Left err -> printError err
+        _ -> return ()
